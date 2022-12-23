381779060dc7906b830fbc428cde1c3e3da8473b
Moussa Zalzala <moussa.zalzala@hotmail.com>
2022-12-23 03:20:05 +0100

    Mapped the rest of the tables More details in Teams chat

--- src/main/java/com/example/project_golf_piper_games/HelloApplication.java
+++ src/main/java/com/example/project_golf_piper_games/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.project_golf_piper_games;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,7 +18,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Pied Piper Database Manager");
+        stage.getIcons().add(new Image("file:Icon.png"));
         stage.setScene(scene);
         stage.show();
     }
